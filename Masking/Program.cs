138eea6ccd41c4bf949b7919269fe2c0fa948839
christensenjoshua <christensen.joshua@outlook.com>
2018-10-16 18:38:13 -0600

    Working

--- Masking/Program.cs
+++ Masking/Program.cs
@@ -10,6 +10,39 @@ namespace Masking
     {
         static void Main(string[] args)
         {
+            decimal number = 1234567890;
+            string mask = "(xxx) xxx-xxxx";
+            char replace = 'x';
+            Console.WriteLine(ApplyMask(number, mask, replace));
+            Console.ReadLine();
+        }
+        static string ApplyMask(decimal num, string mask, char replace)
+        {
+            string output = "";
+            Stack<char> inputs = new Stack<char>();
+            Stack<char> maskChars = new Stack<char>();
+            string numString = num.ToString();
+            for(int i = numString.Length -1; i >= 0; i--)
+            {
+                inputs.Push(numString[i]);
+            }
+            for (int i = mask.Length - 1; i >= 0; i--)
+            {
+                maskChars.Push(mask[i]);
+            }
+            while(inputs.Count != 0 && maskChars.Count != 0)
+            {
+                char thisChar = maskChars.Pop();
+                if(thisChar == replace)
+                {
+                    output += inputs.Pop();
+                }
+                else
+                {
+                    output += thisChar;
+                }
+            }
+            return output;
         }
     }
 }
